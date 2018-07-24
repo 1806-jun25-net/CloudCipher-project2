42fb0b66dae8e8fd3f8dc20919b9dd463659545c
Brandon Kirk <bjkirk94@gmail.com>
2018-07-24 09:45:25 -0400

    Added KeywordRepo with basic methods.  Also added method documentation to both Keyword Repo and AppUserRepo.

--- RestaurantAPI/RestaurantAPI.Library/Repos/AppUserRepo.cs
+++ RestaurantAPI/RestaurantAPI.Library/Repos/AppUserRepo.cs
@@ -16,6 +16,11 @@ namespace RestaurantAPI.Library.Repos
             _db = db ?? throw new ArgumentNullException(nameof(db));
         }
 
+        /// <summary>
+        /// Primary method for retriving all users from the database.  Use parameter to distinguish whether information from junciton tables is required or not.
+        /// </summary>
+        /// <param name="includeAll">Whether to include the information from junction tables or not</param>
+        /// <returns>Returns an IQueryable containing all users in the database.  Use ToList() on the result to make it a usable list.</returns>
         public IQueryable<AppUser> GetUsers(bool includeAll)
         {
             if (includeAll)
@@ -23,6 +28,14 @@ namespace RestaurantAPI.Library.Repos
             return _db.AppUser.AsNoTracking();
         }
 
+        /// <summary>
+        /// Overload for selecting which specific junciton table information is requred for list of users
+        /// </summary>
+        /// <param name="includeBlacklist">Whether to include the list of blacklisted restaurants for that user</param>
+        /// <param name="includeFavorites">Whether to include the list of favorited restaurants for that user</param>
+        /// <param name="includeQueries">Whether to include the list of queries for that user</param>
+        /// <param name="includeOwnedRestaurants">Whether to include the list of restaurants registered as owned by that user</param>
+        /// <returns></returns>
         public IQueryable<AppUser> GetUsers(bool includeBlacklist, bool includeFavorites, bool includeQueries, bool includeOwnedRestaurants)
         {
             IQueryable<AppUser> result = _db.AppUser.AsNoTracking();
@@ -37,6 +50,12 @@ namespace RestaurantAPI.Library.Repos
             return result;
         }
 
+        /// <summary>
+        /// Given a username, returns the AppUser object with that username from the DB.  By default will include all list data
+        /// Throws an exception if username not found in DB
+        /// </summary>
+        /// <param name="username">Username to look up in database</param>
+        /// <returns>AppUser object with specified useranme</returns>
         public AppUser GetUserByUsername(string username)
         {
             if (!DBContainsUsername(username))
@@ -44,17 +63,32 @@ namespace RestaurantAPI.Library.Repos
             return GetUsers(true).First(t => t.Username.Equals(username));
         }
 
+        /// <summary>
+        /// Determines whether a user with the given username exists in the DB
+        /// </summary>
+        /// <param name="username">name of user to search for</param>
+        /// <returns>true if user is found in DB, false otherwise</returns>
         public bool DBContainsUsername(string username)
         {
             return GetUsers(false).Any(t => t.Username.Equals(username));
         }
 
+        /// <summary>
+        /// Adds a new AppUser object to the DB
+        /// Throws an exception if that username is already in use to avoid violating PK constraint.
+        /// Must still call Save() after all DB updates are finished.
+        /// </summary>
+        /// <param name="u">AppUser object to be added to DB</param>
         public void AddUser(AppUser u)
         {
             if (DBContainsUsername(u.Username))
                 throw new DbUpdateException("That username is already in the database.  Usernames must be unique to add a new user", new NotSupportedException());
             _db.Add(u);
         }
+
+        /// <summary>
+        /// Saves changes to DB
+        /// </summary>
         public void Save()
         {
             _db.SaveChanges();
---
+++ RestaurantAPI/RestaurantAPI.Library/Repos/KeywordRepo.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantAPI.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RestaurantAPI.Library.Repos
+{
+    class KeywordRepo
+    {
+        private readonly Project2DBContext _db;
+
+        public KeywordRepo(Project2DBContext db)
+        {
+            _db = db ?? throw new ArgumentNullException(nameof(db));
+        }
+
+        /// <summary>
+        /// Default method for retriving all queried Keywords from the DB.
+        /// </summary>
+        /// <returns>Returns an IQueryable containing all Keyword objects in the database.  Use ToList() on the result to make it a usable list.  Use keyword.Word to access the actual string.</returns>
+        public IQueryable<Keyword> GetKeywords()
+        {
+            return _db.Keyword.AsNoTracking();
+        }
+
+        /// <summary>
+        /// Checks whether the given Keyword already exists in the DB or not.  Overload which takes a Keyword object.
+        /// </summary>
+        /// <param name="kw">Keyword object who's .Word is to be checked for existence in the DB</param>
+        /// <returns>true if Keyword found in DB, falase otherwise</returns>
+        public bool DBContainsKeyword(Keyword kw)
+        {
+            return GetKeywords().Any(t => t.Word.Equals(kw.Word));
+        }
+
+        /// <summary>
+        /// Checks whether the given Keyword already exists in the DB or not.  Overload which takes a string.
+        /// </summary>
+        /// <param name="kw">string to be checked for existence in the DB as a Keyword</param>
+        /// <returns>true if keyword string found in DB, falase otherwise</returns>
+        public bool DBContainsKeyword(string kw)
+        {
+            return GetKeywords().Any(t => t.Word.Equals(kw));
+        }
+
+        /// <summary>
+        /// Adds the given Keyword object to the DB.  Overload which takes the Keyword object.
+        /// Throws an exception if the Keyword already exists in the DB to avoid violating PK constraint.
+        /// Still need to call Save() afterwards to keep changes.
+        /// </summary>
+        /// <param name="u">Keyword object to add to DB</param>
+        public void AddKeyword(Keyword u)
+        {
+            if (DBContainsKeyword(u))
+                throw new DbUpdateException("That keyword is already in the database.  Keywords must be unique.", new NotSupportedException());
+            _db.Add(u);
+        }
+
+        /// <summary>
+        /// Adds the given keyword string to the DB.  Overload which takes a string of keyword to be added.
+        /// Throws an exception if the given string has a Keyword already in the DB to avoid violating PK constraint.
+        /// Still need to call Save() afterwards to keep changes.
+        /// </summary>
+        /// <param name="u">keyword string to add to DB</param>
+        public void AddKeyword(string u)
+        {
+            if (DBContainsKeyword(u))
+                throw new DbUpdateException("That keyword is already in the database.  Keywords must be unique.", new NotSupportedException());
+            _db.Add(new Keyword() { Word = u } );
+        }
+
+        /// <summary>
+        /// Saves changes to DB
+        /// </summary>
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
