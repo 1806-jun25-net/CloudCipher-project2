d91ee20d620e74167599308817ad75612392fa9d
Brandon Kirk <bjkirk94@gmail.com>
2018-07-25 16:39:58 -0400

    Added mapper methods for conversions between user object types.

--- RestaurantAPI/RestaurantAPI.API/Models/Mapper.cs
+++ RestaurantAPI/RestaurantAPI.API/Models/Mapper.cs
@@ -1,6 +1,8 @@
-﻿using RestaurantAPI.Data;
+﻿using RestaurantAPI.API.Models;
+using RestaurantAPI.Data;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace RestaurantAPI.Library
@@ -10,12 +12,30 @@ namespace RestaurantAPI.Library
         // single object conversions
         public static AppUser Map(UserModel otherUser) => new AppUser
         {
-            
+            Username = otherUser.Username,
+            FirstName = otherUser.FirstName,
+            LastName = otherUser.LastName,
+            Email = otherUser.Email,
+            UserType = otherUser.UserType
         };
 
         public static UserModel Map(AppUser otherUser) => new UserModel
         {
-            
+            Username = otherUser.Username,
+            FirstName = otherUser.FirstName,
+            LastName = otherUser.LastName,
+            Email = otherUser.Email,
+            UserType = otherUser.UserType
         };
+
+
+
+        // collection of objects conversions
+
+        public static IEnumerable<UserModel> Map(IEnumerable<AppUser> otherUsers) => otherUsers.Select(Map);
+
+        public static IEnumerable<AppUser> Map(IEnumerable<UserModel> otherUsers) => otherUsers.Select(Map);
+
+
     }
 }
