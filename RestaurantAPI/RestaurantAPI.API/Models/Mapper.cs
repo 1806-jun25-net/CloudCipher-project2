653c104d3640bb10e90a0d05c6242162c59b4b3d
Brandon Kirk <bjkirk94@gmail.com>
2018-07-25 16:36:20 -0400

    Moved Mapper class to the API project per recommendation from Nick

---
+++ RestaurantAPI/RestaurantAPI.API/Models/Mapper.cs
@@ -0,0 +1,21 @@
+﻿using RestaurantAPI.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantAPI.Library
+{
+    public class Mapper
+    {
+        // single object conversions
+        public static AppUser Map(UserModel otherUser) => new AppUser
+        {
+            
+        };
+
+        public static UserModel Map(AppUser otherUser) => new UserModel
+        {
+            
+        };
+    }
+}
