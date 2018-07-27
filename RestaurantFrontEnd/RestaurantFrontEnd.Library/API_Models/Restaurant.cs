a801e1134ff67acaed0bb257e2ae4f5a1f8a0a44
jhc5000 <jhc5000@gmail.com>
2018-07-27 00:58:40 -0400

    integrated MVC-API User model connection

---
+++ RestaurantFrontEnd/RestaurantFrontEnd.Library/API_Models/Restaurant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantFrontEnd.Library.API_Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Hours { get; set; }
+        public string Location { get; set; }
+        public string Location2 { get; set; }
+        public string Owner { get; set; }
+    }
+}
