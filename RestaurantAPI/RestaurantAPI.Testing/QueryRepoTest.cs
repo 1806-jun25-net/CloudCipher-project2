e9dd85fb093ae8a94524e27cf2457593f8ede25b
Brandon Kirk <bjkirk94@gmail.com>
2018-07-28 14:15:27 -0400

    Added tests for existing QueryRepo methods

---
+++ RestaurantAPI/RestaurantAPI.Testing/QueryRepoTest.cs
@@ -0,0 +1,422 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantAPI.Data;
+using RestaurantAPI.Library.Repos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace RestaurantAPI.Testing
+{
+    public class QueryRepoTest
+    {
+        public QueryRepoTest()
+        {
+            var options = new DbContextOptionsBuilder<Project2DBContext>()
+                .UseInMemoryDatabase(databaseName: "StaticFilledQueryDB")
+                .Options;
+            using (var context = new Project2DBContext(options))
+            {
+                //Only initialize the DB with data once
+                if (context.AppUser.ToList().Count == 0)
+                {
+                    context.AppUser.Add(new AppUser { Username = "realUser", FirstName = "a", LastName = "b", Email = "e" });
+                    context.AppUser.Add(new AppUser { Username = "decoyUser1", FirstName = "a", LastName = "b", Email = "e" });
+                    context.AppUser.Add(new AppUser { Username = "decoyUser2", FirstName = "a", LastName = "b", Email = "e" });
+                    context.AppUser.Add(new AppUser { Username = "decoyUser3", FirstName = "a", LastName = "b", Email = "e" });
+
+                    context.Restaurant.Add(new Restaurant { Id = 1, Name = "1", Location = "loc", Owner = "realUser" });
+                    context.Restaurant.Add(new Restaurant { Id = 2, Name = "2", Location = "loc" });
+                    context.Restaurant.Add(new Restaurant { Id = 3, Name = "3", Location = "loc" });
+                    context.Restaurant.Add(new Restaurant { Id = 4, Name = "4", Location = "loc" });
+                    context.Restaurant.Add(new Restaurant { Id = 5, Name = "5", Location = "loc", Owner = "realUser" });
+                    context.Restaurant.Add(new Restaurant { Id = 6, Name = "6", Location = "loc" });
+                    context.Restaurant.Add(new Restaurant { Id = 7, Name = "7", Location = "loc" });
+                    context.Restaurant.Add(new Restaurant { Id = 8, Name = "8", Location = "loc" });
+                    context.Restaurant.Add(new Restaurant { Id = 9, Name = "9", Location = "loc" });
+
+
+                    context.Blacklist.Add(new Blacklist { Username = "realUser", RestaurantId = 2 });
+                    context.Blacklist.Add(new Blacklist { Username = "realUser", RestaurantId = 4 });
+                    context.Blacklist.Add(new Blacklist { Username = "realUser", RestaurantId = 6 });
+
+                    context.Favorite.Add(new Favorite { Username = "realUser", RestaurantId = 1 });
+                    context.Favorite.Add(new Favorite { Username = "realUser", RestaurantId = 3 });
+                    context.Favorite.Add(new Favorite { Username = "realUser", RestaurantId = 5 });
+                    context.Favorite.Add(new Favorite { Username = "realUser", RestaurantId = 7 });
+
+                    context.Keyword.Add(new Keyword { Word = "breakfast" });
+                    context.Keyword.Add(new Keyword { Word = "fast" });
+                    context.Keyword.Add(new Keyword { Word = "food" });
+
+                    context.Query.Add(new Query { Id = 1, Username = "realUser", QueryTime = DateTime.Now });
+                    context.Query.Add(new Query { Id = 2, Username = "realUser", QueryTime = DateTime.Now });
+                    context.Query.Add(new Query { Id = 3, Username = "realUser", QueryTime = DateTime.Now });
+                    context.Query.Add(new Query { Id = 4, Username = "realUser", QueryTime = DateTime.Now });
+                    context.Query.Add(new Query { Id = 5, Username = "realUser", QueryTime = DateTime.Now });
+                    context.Query.Add(new Query { Id = 6, Username = "realUser", QueryTime = DateTime.Now });
+                    context.Query.Add(new Query { Id = 7, Username = "realUser", QueryTime = DateTime.Now });
+                    context.Query.Add(new Query { Id = 8, Username = "realUser", QueryTime = DateTime.Now });
+
+                    context.QueryKeywordJunction.Add(new QueryKeywordJunction { QueryId = 1, Word = "breakfast" });
+                    context.QueryKeywordJunction.Add(new QueryKeywordJunction { QueryId = 1, Word = "fast" });
+                    context.QueryKeywordJunction.Add(new QueryKeywordJunction { QueryId = 1, Word = "food" });
+                    
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        //Testing of GetQueries()
+        [Fact]
+        public void GetQueriesShouldNotThrowExceptionIfDBIsEmpty()
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<Project2DBContext>()
+                .UseInMemoryDatabase(databaseName: "EmptyDB1")
+                .Options;
+
+            bool result = true;
+            QueryRepo qRepo;
+
+            //Act
+            using (var context = new Project2DBContext(options))
+            {
+                qRepo = new QueryRepo(context);
+                try
+                {
+                    qRepo.GetQueries();
+                }
+                catch
+                {
+                    result = false;
+                }
+            }
+            //Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void GetQueriesShouldReturnAListWithProperNumberOfQueriess()
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<Project2DBContext>()
+                .UseInMemoryDatabase(databaseName: "StaticFilledQueryDB")
+                .Options;
+            QueryRepo qRepo;
+            List<Query> qList;
+
+            //Act
+            using (var context = new Project2DBContext(options))
+            {
+                qRepo = new QueryRepo(context);
+                qList = qRepo.GetQueries().ToList();
+            }
+            //Assert
+            Assert.Equal(8, qList.Count);
+        }
+
+        //Testing of DBContainsQuery
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(11)]
+        [InlineData(4232)]
+        [InlineData(67)]
+        [InlineData(324)]
+        public void DBContainsQueryShouldNotThrowExceptionIfDBIsEmpty(int Id)
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<Project2DBContext>()
+                .UseInMemoryDatabase(databaseName: "EmptyDB2")
+                .Options;
+
+            bool result = true;
+            QueryRepo qRepo;
+
+            //Act
+            using (var context = new Project2DBContext(options))
+            {
+                qRepo = new QueryRepo(context);
+                qRepo.DBContainsQuery(Id);
+            }
+            //If exception is throw, test will exit before reaching Assert
+            //Assert
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(11)]
+        [InlineData(4232)]
+        [InlineData(66)]
+        [InlineData(324)]
+        public void DBContainsQueryShouldReturnFalseIfIfDBIsEmpty(int Id)
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<Project2DBContext>()
+                .UseInMemoryDatabase(databaseName: "EmptyDB3")
+                .Options;
+            bool result;
+            QueryRepo qRepo;
+
+            //Act
+            using (var context = new Project2DBContext(options))
+            {
+                qRepo = new QueryRepo(context);
+                result = qRepo.DBContainsQuery(Id);
+            }
+            //If exception is throw, test will exit before reaching Assert
+            //Assert
+            Assert.False(result);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        public void DBContainsQueryShouldReturnTrueIfRestaurantIdInDB(int Id)
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<Project2DBContext>()
+                .UseInMemoryDatabase(databaseName: "StaticFilledQueryDB")
+                .Options;
+            bool result;
+            QueryRepo qRepo;
+
+            //Act
+            using (var context = new Project2DBContext(options))
+            {
+                qRepo = new QueryRepo(context);
+                result = qRepo.DBContainsQuery(Id);
+            }
+            //Assert
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData(11)]
+        [InlineData(4232)]
+        [InlineData(66)]
+        [InlineData(324)]
+        public void DBContainsQueryShouldReturnFalseIfRestaurantIdNotInDB(int Id)
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<Project2DBContext>()
+                .UseInMemoryDatabase(databaseName: "StaticFilledQueryDB")
+                .Options;
+            bool result;
+            QueryRepo qRepo;
+
+            //Act
+            using (var context = new Project2DBContext(options))
+            {
+                qRepo = new QueryRepo(context);
+                result = qRepo.DBContainsQuery(Id);
+            }
+            //Assert
+            Assert.False(result);
+        }
+
+        //Testing of GetQueryByID
+        [Theory]
+        [InlineData(11)]
+        [InlineData(4232)]
+        [InlineData(66)]
+        [InlineData(324)]
+        public void GetQueryByIDShouldThrowExceptionIfIdNotFound(int Id)
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<Project2DBContext>()
+                .UseInMemoryDatabase(databaseName: "StaticFilledQueryDB")
+                .Options;
+
+            bool result = false;
+            QueryRepo qRepo;
+
+            //Act
+            using (var context = new Project2DBContext(options))
+            {
+                qRepo = new QueryRepo(context);
+                try
+                {
+                    qRepo.GetQueryByID(Id);
+                }
+                catch (NotSupportedException)
+                {
+                    result = true;
+                }
+            }
+            //Assert
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        public void GetQueryByIDShouldNotThrowExceptionIfIdIsInDB(int Id)
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<Project2DBContext>()
+                .UseInMemoryDatabase(databaseName: "StaticFilledQueryDB")
+                .Options;
+            bool result = true;
+            QueryRepo qRepo;
+
+            //Act
+            using (var context = new Project2DBContext(options))
+            {
+                qRepo = new QueryRepo(context);
+                qRepo.GetQueryByID(Id);
+            }
+            //If exception is throw, test will exit before reaching Assert
+            //Assert
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        public void GetQueryByIDShouldReturnRestaurantWithMatchingId(int Id)
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<Project2DBContext>()
+                .UseInMemoryDatabase(databaseName: "StaticFilledQueryDB")
+                .Options;
+
+            Query q;
+            QueryRepo qRepo;
+
+            //Act
+            using (var context = new Project2DBContext(options))
+            {
+                qRepo = new QueryRepo(context);
+                q = qRepo.GetQueryByID(Id);
+            }
+
+            //Assert
+            Assert.Equal(Id, q.Id);
+        }
+
+        //Testing of AddQuery
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        public void AddQueryShouldThrowExceptionIfIdAlreadyInDB(int Id)
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<Project2DBContext>()
+                .UseInMemoryDatabase(databaseName: "EmptyAddTestingDB")
+                .Options;
+
+            Query q =  new Query { Id = Id, Username = "tester", QueryTime = DateTime.Now };
+            Query q2 = new Query { Id = Id, Username = "tester", QueryTime = DateTime.Now };
+            QueryRepo qRepo;
+            KeywordRepo kRepo;
+            bool result = false;
+            using (var context = new Project2DBContext(options))
+            {
+                context.Query.Add(q2);
+                context.SaveChanges();
+            }
+
+            //Act
+            using (var context = new Project2DBContext(options))
+            {
+                qRepo = new QueryRepo(context);
+                kRepo = new KeywordRepo(context);
+                try
+                {
+                    qRepo.AddQuery(q, kRepo);
+                }
+                catch (DbUpdateException)
+                {
+                    result = true;
+                }
+            }
+
+            //Assert
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        public void AddRestaurantShouldThrowExceptionIfNameAndLocAlreadyInDB(int Id)
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<Project2DBContext>()
+                .UseInMemoryDatabase(databaseName: "EmptyAddTestingDB")
+                .Options;
+
+            Restaurant r = new Restaurant { Id = 10, Name = Id.ToString(), Location = "loc" };
+            Restaurant r2 = new Restaurant { Id = 11, Name = Id.ToString(), Location = "loc" };
+            RestaurantRepo rRepo;
+            bool result = false;
+            using (var context = new Project2DBContext(options))
+            {
+                context.Restaurant.Add(r2);
+            }
+
+            //Act
+            using (var context = new Project2DBContext(options))
+            {
+                rRepo = new RestaurantRepo(context);
+                try
+                {
+                    rRepo.AddRestaurant(r);
+                }
+                catch (DbUpdateException)
+                {
+                    result = true;
+                }
+            }
+
+            //Assert
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        public void AddRestrauntShouldAddCorrectRestauranttoDB(int Id)
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<Project2DBContext>()
+                .UseInMemoryDatabase(databaseName: "EmptyAddTesting2DB")
+                .Options;
+
+            Restaurant r = new Restaurant { Name = Id.ToString(), Location = "loc" };
+            RestaurantRepo rRepo;
+            Restaurant result;
+
+            //Act
+            using (var context = new Project2DBContext(options))
+            {
+                rRepo = new RestaurantRepo(context);
+                rRepo.AddRestaurant(r);
+                result = context.Restaurant.Find(r.Id);
+            }
+
+            //Assert
+            Assert.Equal(r, result);
+        }
+    }
+}
