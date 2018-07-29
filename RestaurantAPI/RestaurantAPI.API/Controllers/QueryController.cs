60c910e363604c18716a13676a346a6567744583
Brandon Kirk <bjkirk94@gmail.com>
2018-07-29 12:35:35 -0400

    Iplemented basic methods for QueryController.

--- RestaurantAPI/RestaurantAPI.API/Controllers/QueryController.cs
+++ RestaurantAPI/RestaurantAPI.API/Controllers/QueryController.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RestaurantAPI.API.Models;
+using RestaurantAPI.Data;
+using RestaurantAPI.Library;
+using RestaurantAPI.Library.Repos;
 
 namespace RestaurantAPI.API.Controllers
 {
@@ -11,26 +15,84 @@ namespace RestaurantAPI.API.Controllers
     [ApiController]
     public class QueryController : ControllerBase
     {
+        public QueryController(IAppUserRepo AppRepo, IKeywordRepo KeyRepo, IQueryRepo QRepo, IRestaurantRepo RestRepo)
+        {
+            Arepo = AppRepo;
+            Krepo = KeyRepo;
+            Qrepo = QRepo;
+            Rrepo = RestRepo;
+        }
+
+        public IAppUserRepo Arepo { get; set; }
+        public IKeywordRepo Krepo { get; set; }
+        public IQueryRepo Qrepo { get; set; }
+        public IRestaurantRepo Rrepo { get; set; }
+
+
         // GET: api/Query
         [HttpGet]
-        public IEnumerable<string> Get()
+        public ActionResult<List<QueryModel>> Get()
         {
-            return new string[] { "value1", "value2" };
+            var queryList = Qrepo.GetQueries();
+            if (queryList == null)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            var queryModelList = Mapper.Map(queryList);
+
+            return queryModelList.ToList();
         }
 
         // GET: api/Query/5
         [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
+        public ActionResult<QueryModel> Get(int id)
         {
-            return "value";
+            Query q;
+            try
+            {
+                q = Qrepo.GetQueryByID(id);
+            }
+
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest);
+            }
+
+            return Mapper.Map(q);
         }
 
         // POST: api/Query
         [HttpPost]
-        public void Post([FromBody] string value)
+        public ActionResult<List<RestaurantModel>> Post([FromBody] QueryModel queryM)
         {
+            //Add query to DB
+            try
+            {
+                Qrepo.AddQuery(Mapper.Map(queryM), (KeywordRepo)Krepo);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status400BadRequest);
+            }
+            //Execute request of Google Search API to get some list of RestaurantModels back
+            //TODO:  connect this controller with Google Search API functionality
+            List<RestaurantModel> queryResults = new List<RestaurantModel>();
+            //Add any new restaurants to DB, and register any new keywords to existing restaurants
+
+            try
+            {
+                Rrepo.AddNewRestaurants(Mapper.Map(queryResults).ToList(), queryM.Keywords);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            return queryResults;
         }
 
+        /*  No need for update or delete methods for queries
+         *  
         // PUT: api/Query/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
@@ -42,5 +104,7 @@ namespace RestaurantAPI.API.Controllers
         public void Delete(int id)
         {
         }
+        *
+        */
     }
 }
