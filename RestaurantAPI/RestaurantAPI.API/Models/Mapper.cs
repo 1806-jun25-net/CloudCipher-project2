91ccf89746131ff63af31b60112019f1364dee7c
Brandon Kirk <bjkirk94@gmail.com>
2018-07-28 23:04:54 -0400

    Created model for queries as well as accompanying Mapper methods

--- RestaurantAPI/RestaurantAPI.API/Models/Mapper.cs
+++ RestaurantAPI/RestaurantAPI.API/Models/Mapper.cs
@@ -50,6 +50,33 @@ namespace RestaurantAPI.Library
             Owner = other.Owner
         };
 
+        public static QueryModel Map(Query other) => new QueryModel
+        {
+            Id = other.Id,
+            Username = other.Username,
+            Location = other.Location,
+            Location2 = other.Location2,
+            Radius = (int)other.Radius,
+            QueryTime = other.QueryTime,
+            ReservationTime = (DateTime)other.ReservationTime,
+            Keywords = other.QueryKeywordJunction.Select(q => q.Word).ToList()
+        };
+
+        public static Query Map(QueryModel other) => new Query
+        {
+            Id = other.Id,
+            Username = other.Username,
+            Location = other.Location,
+            Location2 = other.Location2,
+            Radius = other.Radius,
+            QueryTime = other.QueryTime,
+            ReservationTime = other.ReservationTime,
+            QueryKeywordJunction = other.Keywords.Select( k => new QueryKeywordJunction() { Word = k, QueryId = other.Id }).ToList()
+        };
+
+        public static string Map(Keyword other) => other.Word;
+        public static Keyword Map(string other) => new Keyword { Word = other };
+
 
         // collection of objects conversions
 
@@ -59,6 +86,11 @@ namespace RestaurantAPI.Library
         public static IEnumerable<RestaurantModel> Map(IEnumerable<Restaurant> others) => others.Select(Map);
         public static IEnumerable<Restaurant> Map(IEnumerable<RestaurantModel> others) => others.Select(Map);
 
+        public static IEnumerable<QueryModel> Map(IEnumerable<Query> others) => others.Select(Map);
+        public static IEnumerable<Query> Map(IEnumerable<QueryModel> others) => others.Select(Map);
+
+        public static IEnumerable<string> Map(IEnumerable<Keyword> others) => others.Select(Map);
+        public static IEnumerable<Keyword> Map(IEnumerable<string> others) => others.Select(Map);
 
     }
 }
---
+++ RestaurantAPI/RestaurantAPI.API/Models/QueryModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.API.Models
+{
+    public class QueryModel
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Location { get; set; }
+        public string Location2 { get; set; }
+        public int Radius { get; set; }
+        public DateTime QueryTime { get; set; }
+        public DateTime ReservationTime { get; set; }
+
+        public List<string> Keywords { get; set; }
+    }
+}
