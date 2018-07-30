c1ca2a8e493cd8d3c20a24b64f05773ca3a74ad4
Brandon Kirk <bjkirk94@gmail.com>
2018-07-30 00:12:05 -0400

    Removed unused UserType field from User.  Also added some minor data annotations.

--- RestaurantFrontEnd/RestaurantFrontEnd.Library/API_Models/User.cs
+++ RestaurantFrontEnd/RestaurantFrontEnd.Library/API_Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace RestaurantFrontEnd.Library.API_Models
@@ -7,10 +8,15 @@ namespace RestaurantFrontEnd.Library.API_Models
     public class User
     {
         public string Username { get; set; }
+
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
+        [Display(Name = "Email")]
         public string Email { get; set; }
-        public int UserType { get; set; }
 
     }
 }
