230f0d35c72efd44721dd65b762259739c2f051e
Lyke <Lyke@LYKESLAPTOP-HP>
2018-07-30 12:59:38 -0400

    added query and queryresult classes to send query results to api

---
+++ RestaurantFrontEnd/RestaurantFrontEnd.Library/API_Models/QueryResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantFrontEnd.Library.API_Models
+{
+    public class QueryResult
+    {
+        public Query QueryObject { get; set; }
+        public List<string> Restaurants { get; set; }
+
+    }
+}
