5a0023d76a18aa568e4281357e3101896b981b32
Brandon Kirk <bjkirk94@gmail.com>
2018-07-30 16:19:22 -0400

    Changed front end model classes to match those in API after DB change.

--- RestaurantFrontEnd/RestaurantFrontEnd.Library/API_Models/Query.cs
+++ RestaurantFrontEnd/RestaurantFrontEnd.Library/API_Models/Query.cs
@@ -9,11 +9,10 @@ namespace RestaurantFrontEnd.Library.API_Models
 
         public int Id { get; set; }
         public string Username { get; set; }
-        public string Location { get; set; }
-        public string Location2 { get; set; }
+        public string Lat { get; set; }
+        public string Lon { get; set; }
         public int Radius { get; set; }
         public DateTime QueryTime { get; set; }
-        public DateTime ReservationTime { get; set; }
 
         public List<string> Keywords { get; set; }
 
--- RestaurantFrontEnd/RestaurantFrontEnd.Library/API_Models/Restaurant.cs
+++ RestaurantFrontEnd/RestaurantFrontEnd.Library/API_Models/Restaurant.cs
@@ -6,12 +6,14 @@ namespace RestaurantFrontEnd.Library.API_Models
 {
     public class Restaurant
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string Name { get; set; }
-        public string Phone { get; set; }
         public string Hours { get; set; }
-        public string Location { get; set; }
-        public string Location2 { get; set; }
+        public string Lat { get; set; }
+        public string Lon { get; set; }
+        public string Address { get; set; }
+        public decimal Rating { get; set; }
+        public decimal PriceLevel { get; set; }
         public string Owner { get; set; }
     }
 }
