e4d767a7e6852175b446946bf88ba97dd2e76b2c
Brandon Kirk <bjkirk94@gmail.com>
2018-07-31 19:42:19 -0400

    Added auto generated blacklist and favorites controllers

---
+++ RestaurantAPI/RestaurantAPI.API/Controllers/BlacklistController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlacklistController : ControllerBase
+    {
+        // GET: api/Blacklist
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Blacklist/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Blacklist
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Blacklist/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
