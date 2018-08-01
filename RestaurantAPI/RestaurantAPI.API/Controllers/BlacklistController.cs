0b85d7f5dc2b1090dabb872435603cb8a7ed9198
Lyke <Lyke@LYKESLAPTOP-HP>
2018-08-01 00:45:11 -0400

    Completed favorites and blacklist controller

--- RestaurantAPI/RestaurantAPI.API/Controllers/BlacklistController.cs
+++ RestaurantAPI/RestaurantAPI.API/Controllers/BlacklistController.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RestaurantAPI.API.Models;
+using RestaurantAPI.Library;
+using RestaurantAPI.Library.Repos;
 
 namespace RestaurantAPI.API.Controllers
 {
@@ -11,12 +15,29 @@ namespace RestaurantAPI.API.Controllers
     [ApiController]
     public class BlacklistController : ControllerBase
     {
+        public BlacklistController(IAppUserRepo AppRepo, IKeywordRepo KeyRepo, IQueryRepo QRepo, IRestaurantRepo RestRepo)
+        {
+            Arepo = AppRepo;
+            Krepo = KeyRepo;
+            Qrepo = QRepo;
+            Rrepo = RestRepo;
+        }
+
+        public IAppUserRepo Arepo { get; set; }
+        public IKeywordRepo Krepo { get; set; }
+        public IQueryRepo Qrepo { get; set; }
+        public IRestaurantRepo Rrepo { get; set; }
+
+
         //Return list of all favorited Restaurants for a given user
         // GET: api/Blacklist
         [HttpGet]
-        public IEnumerable<string> Get()
+        public ActionResult<List<RestaurantModel>> Get()
         {
-            return new string[] { "value1", "value2" };
+            Arepo.GetFavoritesForUser(User.Identity.Name);
+
+            return Mapper.Map(Arepo.GetFavoritesForUser(User.Identity.Name)).ToList();
+
         }
 
         //Unused
@@ -30,8 +51,23 @@ namespace RestaurantAPI.API.Controllers
         //Given a restaurant id as a parameter, add the restaurant to the current user's favorites
         // POST: api/Blacklist
         [HttpPost]
-        public void Post([FromBody] string value)
+        [Authorize]
+        public IActionResult Create([FromBody] string value)
         {
+
+            try
+            {
+                Arepo.AddRestaurantToBlacklist(User.Identity.Name, value, (RestaurantRepo)Rrepo);
+            }
+
+            catch
+            {
+                return StatusCode(StatusCodes.Status400BadRequest);
+            }
+
+            Rrepo.Save();
+
+            return CreatedAtRoute("GetBlacklist", new { Id = value }, value);
         }
 
         //Unused
@@ -44,8 +80,23 @@ namespace RestaurantAPI.API.Controllers
         //Given a restaurant id as a parameter, remove the restaurant from the current user's favorites
         // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        [Authorize]
+        public IActionResult Delete([FromBody]string value)
         {
+
+            try
+            {
+                Arepo.RemoveRestaurantFromBlacklist(User.Identity.Name, value, (RestaurantRepo)Rrepo);
+            }
+
+            catch
+            {
+                return StatusCode(StatusCodes.Status400BadRequest);
+            }
+
+            Rrepo.Save();
+
+            return CreatedAtRoute("RemoveBlacklist", new { Id = value }, value);
         }
     }
 }
