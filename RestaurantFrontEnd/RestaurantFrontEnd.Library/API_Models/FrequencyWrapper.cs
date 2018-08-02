32285e0bf54def1c2aa1cd6ecfe3a78bb51c0195
Brandon Kirk <bjkirk94@gmail.com>
2018-08-02 13:41:25 -0400

    Corrected some of sonarcloud code smells in front end.

--- RestaurantFrontEnd/RestaurantFrontEnd.Library/API_Models/FrequencyWrapper.cs
+++ RestaurantFrontEnd/RestaurantFrontEnd.Library/API_Models/FrequencyWrapper.cs
@@ -7,7 +7,7 @@ namespace RestaurantFrontEnd.Library.API_Models
     //Object to potentially be used to pass objects to the front end along with their frequency for analytics
     public class FrequencyWrapper<T>
     {
-        public T obj;
-        public int frequency;
+        public T Obj { get; set; }
+        public int Frequency { get; set; }
     }
 }
