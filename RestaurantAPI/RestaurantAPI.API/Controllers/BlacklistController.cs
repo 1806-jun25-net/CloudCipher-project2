4f2c29914a112f0f9fe123c6250672c681017400
Lyke <Lyke@LYKESLAPTOP-HP>
2018-08-04 22:35:05 -0400

    added functionality to controller to exclude blacklisted restaurants

--- RestaurantAPI/RestaurantAPI.API/Controllers/BlacklistController.cs
+++ RestaurantAPI/RestaurantAPI.API/Controllers/BlacklistController.cs
@@ -31,7 +31,7 @@ namespace RestaurantAPI.API.Controllers
         public IRestaurantRepo Rrepo { get; set; }
         private static readonly Logger logger = LogManager.GetCurrentClassLogger();
 
-        //Return list of all favorited Restaurants for a given user
+        //Return list of all blacklisted Restaurants for a given user
         // GET: api/Blacklist
         [ProducesResponseType(401)]
         [ProducesResponseType(500)]
@@ -42,7 +42,7 @@ namespace RestaurantAPI.API.Controllers
             //Since this method is authorized by Identity, it will automatically handle returning 401 if user isn't logged in.
             try
             {
-                return Mapper.Map(await Arepo.GetFavoritesForUserAsync(User.Identity.Name)).ToList();
+                return Mapper.Map(await Arepo.GetBlacklistForUserAsync(User.Identity.Name)).ToList();
             }
             catch (Exception e)
             {
@@ -72,7 +72,7 @@ namespace RestaurantAPI.API.Controllers
             }
         }
 
-        //Given a restaurant id as a parameter, add the restaurant to the current user's favorites
+        //Given a restaurant id as a parameter, add the restaurant to the current user's blacklist
         // POST: api/Blacklist
         [ProducesResponseType(400)]
         [ProducesResponseType(401)]
@@ -105,7 +105,7 @@ namespace RestaurantAPI.API.Controllers
         }
 
 
-        //Given a restaurant id as a parameter, remove the restaurant from the current user's favorites
+        //Given a restaurant id as a parameter, remove the restaurant from the current user's blacklist
         // DELETE: api/Blacklist/5
         [ProducesResponseType(400)]
         [ProducesResponseType(401)]
