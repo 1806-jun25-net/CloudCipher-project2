2b3596da151b9e7b8d387482f4f6b14925fcc664
Brandon Kirk <bjkirk94@gmail.com>
2018-08-06 02:39:04 -0400

    refactored api.service in hopes of reducing code smells.

--- RestaurantAngular/src/app/api.service.ts
+++ RestaurantAngular/src/app/api.service.ts
@@ -7,23 +7,39 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class ApiService {
   //apiUrl: string = "https://cors-anywhere.herokuapp.com/https://cloudcipher-restrauntrecommendations.azurewebsites.net/api/";
-  apiUrl: string = "https://cloudcipher-restrauntrecommendations.azurewebsites.net/api/";
-  //apiUrl: string = "http://localhost:58756/api/";
+  //apiUrl: string = "https://cloudcipher-restrauntrecommendations.azurewebsites.net/api/";
+  //apiUrl: string= "https://restaurantrecommendationsapi.azurewebsites.net/api/";
+  apiUrl: string = "http://localhost:58756/api/";
 
   //Create an HttpClient property equal to parameter taken in
   constructor(private httpClient: HttpClient) { }
+
+  getRequestWithoutCredentials(
+    url: string,
+    success,
+    failure
+  ) {
+    let request = this.httpClient.get(url);
+    let promise = request.toPromise();
+    promise.then(success,failure);
+  }
+
+  getRequestWithCredentials(
+    url: string,
+    success,
+    failure
+  ) {
+    let request = this.httpClient.get(url, { withCredentials: true});
+    let promise = request.toPromise();
+    promise.then(success,failure);
+  }
 
    //get all restaurants in db
   getRestaurants(
     success,
     failure
   ) {
-    let url = this.apiUrl+"restaurant";
-    let request = this.httpClient.get(url);
-    let promise = request.toPromise();
-
-    promise.then(success,failure);
-
+    this.getRequestWithoutCredentials(this.apiUrl+"restaurant", success, failure);
   }
 
   //gets a single restaurant matching the id given
@@ -32,11 +48,7 @@ export class ApiService {
     success,
     failure
   ) {
-    let url = this.apiUrl+"restaurant/"+rId;
-    let request = this.httpClient.get(url);
-    let promise = request.toPromise();
-
-    promise.then(success,failure);
+    this.getRequestWithoutCredentials(this.apiUrl+"restaurant/"+rId, success, failure);
   }
 
   //gets all restaurants matching the search string
@@ -45,12 +57,7 @@ export class ApiService {
     success,
     failure
   ) {
-    console.log(sessionStorage.getItem("AccountKey"));
-    let url = this.apiUrl+"BrowseRestaurant/"+searchText;
-    let request = this.httpClient.get(url, { withCredentials: true});
-    let promise = request.toPromise();
-
-    promise.then(success, failure);
+    this.getRequestWithCredentials(this.apiUrl+"BrowseRestaurant/"+searchText, success, failure);
   }
 
   getBlacklistedForRestaurant(
@@ -58,12 +65,7 @@ export class ApiService {
     success,
     failure
   ) {
-    let url = this.apiUrl+"blacklistAnalytics/"+rId;
-    //let request = this.httpClient.get(url);
-    let request = this.httpClient.get(url);
-    let promise = request.toPromise();
-
-    promise.then(success, failure);
+    this.getRequestWithoutCredentials(this.apiUrl+"blacklistAnalytics/"+rId, success, failure);
   }
 
   getFavoritedForRestaurant(
@@ -71,11 +73,7 @@ export class ApiService {
     success,
     failure
   ) {
-    let url = this.apiUrl+"favoritesAnalytics/"+rId;
-    let request = this.httpClient.get(url);
-    let promise = request.toPromise();
-
-    promise.then(success, failure);
+    this.getRequestWithoutCredentials(this.apiUrl+"favoritesAnalytics/"+rId, success, failure);
   }
 
   getIfRestaurantIsFavorite(
@@ -83,10 +81,7 @@ export class ApiService {
     success,
     failure
   ) {
-    let url = this.apiUrl+"favorites/"+rId;
-    let request = this.httpClient.get(url, { withCredentials: true });
-    let promise = request.toPromise();
-    promise.then(success, failure);
+    this.getRequestWithCredentials(this.apiUrl+"favorites/"+rId, success, failure);
   }
 
   getIfRestaurantIsBlacklisted(
@@ -94,30 +89,21 @@ export class ApiService {
     success,
     failure
   ) {
-    let url = this.apiUrl+"blacklist/"+rId;
-    let request = this.httpClient.get(url, { withCredentials: true });
-    let promise = request.toPromise();
-    promise.then(success, failure);
+    this.getRequestWithCredentials(this.apiUrl+"blacklist/"+rId, success, failure);
   }
 
   getFavoritesForUser(
     success,
     failure
   ) {
-    let url = this.apiUrl+"favorites";
-    let request = this.httpClient.get(url, { withCredentials: true });
-    let promise = request.toPromise();
-    promise.then(success, failure);
+    this.getRequestWithCredentials(this.apiUrl+"favorites", success, failure);
   }
 
   getBlacklistedForUser(
     success,
     failure
   ) {
-    let url = this.apiUrl+"blacklist";
-    let request = this.httpClient.get(url, { withCredentials: true });
-    let promise = request.toPromise();
-    promise.then(success, failure);
+    this.getRequestWithCredentials(this.apiUrl+"blacklist", success, failure);
   }
 
 }
