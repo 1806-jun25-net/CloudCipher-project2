52aef0a5c1b93d3b2b4880c3b313b83ca99cf3d7
Brandon Kirk <bjkirk94@gmail.com>
2018-08-06 07:00:07 -0400

    Changed live version back to using Azure DB

--- RestaurantAngular/src/app/authentication.service.ts
+++ RestaurantAngular/src/app/authentication.service.ts
@@ -10,9 +10,9 @@ import { Account } from './models/account';
 
 export class AuthenticationService {
   //apiUrl: string = "https://cors-anywhere.herokuapp.com/https://cloudcipher-restrauntrecommendations.azurewebsites.net/api/";
-  //apiUrl: string = "https://cloudcipher-restrauntrecommendations.azurewebsites.net/api/";
+  apiUrl: string = "https://cloudcipher-restrauntrecommendations.azurewebsites.net/api/";
   //apiUrl: string= "https://restaurantrecommendationsapi.azurewebsites.net/api/";
-  apiUrl: string = "http://localhost:58756/api/";
+  //apiUrl: string = "http://localhost:58756/api/";
   mvcUrl: string = "https://cloudcipher-restaurantrecommendationsfrontend.azurewebsites.net/";
 
 
